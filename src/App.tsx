26ad203b27e22f53804a4cd79782c4b6908fdf3e
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-03-31 04:39:02 +0900

    play and pause btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {motion} from "motion/react";
+import {useState} from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -66,9 +67,7 @@ const buttonVariants = {
       duration: 0.3,
     },
   },
-}
-
-const Svg = styled.svg``;
+};
 
 const RecordWrapper = styled.div`
   display: flex;
@@ -91,6 +90,7 @@ const RecordNumber = styled.div`
 `;
 
 function App() {
+  const [play, setPlay] = useState(false);
   return (
     <Container>
       <Title>Pomodoro</Title>
@@ -104,21 +104,30 @@ function App() {
         </NumberBox>
       </NumberWrapper>
       <ButtonWrapper>
-        <Button variants={buttonVariants} whileHover="hover">
-          <Svg data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path
-              clip-rule="evenodd"
-              fill-rule="evenodd"
-              d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm6.39-2.908a.75.75 0 0 1 .766.027l3.5 2.25a.75.75 0 0 1 0 1.262l-3.5 2.25A.75.75 0 0 1 8 12.25v-4.5a.75.75 0 0 1 .39-.658Z"></path>
-          </Svg>
+        <Button onClick={() => setPlay(!play)} variants={buttonVariants} whileHover="hover">
+          {!play ? (
+            <svg data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+              <path
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm6.39-2.908a.75.75 0 0 1 .766.027l3.5 2.25a.75.75 0 0 1 0 1.262l-3.5 2.25A.75.75 0 0 1 8 12.25v-4.5a.75.75 0 0 1 .39-.658Z"></path>
+            </svg>
+          ) : (
+            <svg data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+              <path
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm5-2.25A.75.75 0 0 1 7.75 7h.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-.75.75h-.5a.75.75 0 0 1-.75-.75v-4.5Zm4 0a.75.75 0 0 1 .75-.75h.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-.75.75h-.5a.75.75 0 0 1-.75-.75v-4.5Z"></path>
+            </svg>
+          )}
         </Button>
         <Button variants={buttonVariants} whileHover="hover">
-          <Svg data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <svg data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path
               clip-rule="evenodd"
               fill-rule="evenodd"
               d="M15.312 11.424a5.5 5.5 0 0 1-9.201 2.466l-.312-.311h2.433a.75.75 0 0 0 0-1.5H3.989a.75.75 0 0 0-.75.75v4.242a.75.75 0 0 0 1.5 0v-2.43l.31.31a7 7 0 0 0 11.712-3.138.75.75 0 0 0-1.449-.39Zm1.23-3.723a.75.75 0 0 0 .219-.53V2.929a.75.75 0 0 0-1.5 0V5.36l-.31-.31A7 7 0 0 0 3.239 8.188a.75.75 0 1 0 1.448.389A5.5 5.5 0 0 1 13.89 6.11l.311.31h-2.432a.75.75 0 0 0 0 1.5h4.243a.75.75 0 0 0 .53-.219Z"></path>
-          </Svg>
+          </svg>
         </Button>
       </ButtonWrapper>
       <RecordWrapper>
